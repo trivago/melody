a0f90508722b30d1871685aef3f66ff6a2e483e1
Patrick Gotthardt <pago@users.noreply.github.com>
2018-07-02 11:06:04 +0200

    Introduce new skip-if block (#29)
    * Introduce new skip-if block
    
    * Add plugin to coverage report
    
    * Improve error handling for unknown targets
    
    * increase bundle size

---
+++ packages/melody-plugin-skip-if/__tests__/SkipIfSpec.js
@@ -0,0 +1,49 @@
+import { compile, toString } from 'melody-compiler';
+import { extension as coreExtension } from 'melody-extension-core';
+import idomPlugin from 'melody-plugin-idom';
+import skipIfPlugin from '../src';
+
+test('skip if client should skip content', () => {
+    expect(
+        render(`<div>{% skip if client %}hello world{% endskip %}</div>`)
+    ).toMatchSnapshot();
+});
+
+test('skip if server should be ignored', () => {
+    expect(
+        render(`<div>{% skip if server %}hello world{% endskip %}</div>`)
+    ).toMatchSnapshot();
+});
+
+test('skip if defined should check if parent element is defined', () => {
+    expect(
+        render(`<x-div>{% skip if defined %}hello world{% endskip %}</x-div>`)
+    ).toMatchSnapshot();
+});
+
+test('skip if defined should fail if parent element is not a custom element', () => {
+    expect(
+        render(`<div>{% skip if defined %}hello world{% endskip %}</div>`)
+    ).toMatchSnapshot();
+});
+
+test('skip if defined should fail if target is unknown', () => {
+    expect(
+        render(`<div>{% skip if notHavingFun %}hello world{% endskip %}</div>`)
+    ).toMatchSnapshot();
+});
+
+function render(template) {
+    try {
+        const jsTemplate = compile(
+            'test.twig',
+            template,
+            coreExtension,
+            idomPlugin,
+            skipIfPlugin
+        );
+        return toString(jsTemplate, template).code;
+    } catch (e) {
+        return e.message;
+    }
+}

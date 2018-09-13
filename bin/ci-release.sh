339883251207c22307fe0c499df0103c608fae5b
Frank van Gemeren <frvge@users.noreply.github.com>
2018-09-13 14:59:06 +0200

    chore: Update bundlesize, add secret token, remove npm and lerna warn… (#65)
    * chore: Update bundlesize, add secret token, remove npm and lerna warnings
    * chore: Force non-interactive 'yes' for lerna publish

--- bin/ci-release.sh
+++ bin/ci-release.sh
@@ -19,13 +19,12 @@ fi
 if [ "$TRAVIS_BRANCH" = "master" ]; then
   # RELEASE AS @NEXT
   yarn install && \
-    yarn lerna publish --skip-git --exact --force-publish=* --canary=commit --npm-tag=next --registry https://registry.npmjs.org
+    yarn lerna publish --skip-git --exact --force-publish=* --canary=commit --yes --npm-tag=next --registry https://registry.npmjs.org
   exit 0
 else
   # CANARY RELEASE WITH COMMIT NAME
   # @commit
   yarn install && \
-    yarn lerna publish --skip-git --exact --force-publish=*  --canary=commit --registry https://registry.npmjs.org
+    yarn lerna publish --skip-git --exact --force-publish=*  --canary=commit --yes --registry https://registry.npmjs.org
   exit 0
 fi
-

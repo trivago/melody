1b4b1e6f3c2968dc0760d01af3ff446107ef19e7
Patrick Gotthardt <patrick.gotthardt@trivago.com>
2018-12-19 05:33:27 +0100

    Add useAtom hook

--- packages/melody-hooks/src/util/hooks.js
+++ packages/melody-hooks/src/util/hooks.js
@@ -23,6 +23,13 @@ export const setCurrentComponent = c => {
     currentComponent = c;
 };
 
+export const getCurrentComponent = () => {
+    if (!currentComponent) {
+        throw new Error('Cannot use hooks outside of component functions');
+    }
+    return currentComponent;
+};
+
 export const unsetCurrentComponent = () => {
     currentComponent = null;
 };

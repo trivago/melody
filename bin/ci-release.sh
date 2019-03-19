48d14694065bed762638686457de2eb4b866c050
pago <patrick.gotthardt@trivago.com>
2019-03-19 12:55:02 +0100

    Update publish script

--- bin/ci-release.sh
+++ bin/ci-release.sh
@@ -19,12 +19,12 @@ fi
 if [ "$TRAVIS_BRANCH" = "master" ]; then
   # RELEASE AS @NEXT
   yarn install && \
-    yarn lerna publish --no-git-tag-version --no-push --no-git-reset --exact --force-publish=* --canary=commit --yes --dist-tag=next --registry https://registry.npmjs.org
+    yarn lerna publish --no-git-tag-version --no-push --no-git-reset --exact --force-publish=* --canary --yes --dist-tag next --registry https://registry.npmjs.org
   exit 0
 else
   # CANARY RELEASE WITH COMMIT NAME
   # @commit
   yarn install && \
-    yarn lerna publish --no-git-tag-version --no-push --no-git-reset --exact --force-publish=*  --canary=commit --yes --registry https://registry.npmjs.org
+    yarn lerna publish --no-git-tag-version --no-push --no-git-reset --exact --force-publish=*  --canary --yes --dist-tag prerelease --registry https://registry.npmjs.org
   exit 0
 fi

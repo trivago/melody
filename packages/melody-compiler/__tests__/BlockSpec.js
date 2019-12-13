01279db5b6b7082a49e5be4ee4eefd74c1dc1b4d
tbartel <thomas.bartel@trivago.com>
2019-12-13 06:23:02 +0100

    Add whitespace trimming information to BlockStatement

---
+++ packages/melody-compiler/__tests__/BlockSpec.js
@@ -0,0 +1,38 @@
+/**
+ * Copyright 2019 trivago N.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { parse } from 'melody-parser';
+import { extension } from 'melody-extension-core';
+
+describe('block', function() {
+    test('should parse embracing block statement', function() {
+        const node = parse(
+            `{% block hello -%}
+            <div key="1" class="test">
+                <span>Hello</span>
+            </div>
+        {%- endblock %}`,
+            { applyWhitespaceTrimming: false },
+            extension
+        );
+        expect(node.expressions[0]).toMatchSnapshot();
+    });
+
+    test('should parse single-line block', function() {
+        const node = parse(`{%- block bar foo %}`, extension);
+        expect(node.expressions[0]).toMatchSnapshot();
+    });
+});

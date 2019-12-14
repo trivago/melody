21de1ba511a785cbad88b7c8e131c7000365f009
tbartel <thomas.bartel@trivago.com>
2019-12-14 14:53:18 +0100

    Add whitespace trimming information to if statement

---
+++ packages/melody-compiler/__tests__/IfSpec.js
@@ -0,0 +1,61 @@
+/**
+ * Copyright 2019 trivago N.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { parse } from 'melody-parser';
+import { extension } from 'melody-extension-core';
+
+describe('if', function() {
+    test('should parse simple if-endif', function() {
+        const node = parse(
+            `{% if test -%}
+                Dog
+            {%- endif %}`,
+            { applyWhitespaceTrimming: false },
+            extension
+        );
+        expect(node.expressions[0]).toMatchSnapshot();
+    });
+
+    test('should parse simple if-else-endif', function() {
+        const node = parse(
+            `{% if foo %}
+            Foo
+        {%- else -%}
+            Bar
+        {% endif %}`,
+            { applyWhitespaceTrimming: false },
+            extension
+        );
+        expect(node.expressions[0]).toMatchSnapshot();
+    });
+
+    test('should parse if-elseif-elseif-endif', function() {
+        const node = parse(
+            `{% if dog -%}
+            Dog
+        {%- elseif cat -%}
+            Cat
+        {%- elseif bird -%}
+            Bird
+        {%- else %}
+            Other
+        {%- endif -%}`,
+            { applyWhitespaceTrimming: false },
+            extension
+        );
+        expect(node.expressions[0]).toMatchSnapshot();
+    });
+});
--- packages/melody-extension-core/src/parser/if.js
+++ packages/melody-extension-core/src/parser/if.js
@@ -13,7 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { Types, setStartFromToken, setEndFromToken } from 'melody-parser';
+import {
+    Types,
+    setStartFromToken,
+    setEndFromToken,
+    hasTagStartTokenTrimLeft,
+    hasTagEndTokenTrimRight,
+} from 'melody-parser';
 import { IfStatement } from './../types';
 
 export const IfParser = {
@@ -24,25 +30,41 @@ export const IfParser = {
             alternate = null;
 
         tokens.expect(Types.TAG_END);
+        const ifTagEndToken = tokens.la(-1);
 
         const ifStatement = new IfStatement(
             test,
             parser.parse(matchConsequent).expressions
         );
 
+        let elseTagStartToken,
+            elseTagEndToken,
+            elseifTagStartToken,
+            elseifTagEndToken;
+
         do {
             if (tokens.nextIf(Types.SYMBOL, 'else')) {
+                elseTagStartToken = tokens.la(-2);
                 tokens.expect(Types.TAG_END);
+                elseTagEndToken = tokens.la(-1);
                 (alternate || ifStatement).alternate = parser.parse(
                     matchAlternate
                 ).expressions;
             } else if (tokens.nextIf(Types.SYMBOL, 'elseif')) {
+                elseifTagStartToken = tokens.la(-2);
                 test = parser.matchExpression();
                 tokens.expect(Types.TAG_END);
+                elseifTagEndToken = tokens.la(-1);
                 const consequent = parser.parse(matchConsequent).expressions;
                 alternate = (
                     alternate || ifStatement
                 ).alternate = new IfStatement(test, consequent);
+                alternate.trimLeft = hasTagStartTokenTrimLeft(
+                    elseifTagStartToken
+                );
+                alternate.trimRightIf = hasTagEndTokenTrimRight(
+                    elseifTagEndToken
+                );
             }
 
             if (tokens.nextIf(Types.SYMBOL, 'endif')) {
@@ -50,8 +72,21 @@ export const IfParser = {
             }
         } while (!tokens.test(Types.EOF));
 
+        const endifTagStartToken = tokens.la(-2);
+
         setStartFromToken(ifStatement, token);
         setEndFromToken(ifStatement, tokens.expect(Types.TAG_END));
+
+        ifStatement.trimRightIf = hasTagEndTokenTrimRight(ifTagEndToken);
+        ifStatement.trimLeftElse = !!(
+            elseTagStartToken && hasTagStartTokenTrimLeft(elseTagStartToken)
+        );
+        ifStatement.trimRightElse = !!(
+            elseTagEndToken && hasTagEndTokenTrimRight(elseTagEndToken)
+        );
+        ifStatement.trimLeftEndif = hasTagStartTokenTrimLeft(
+            endifTagStartToken
+        );
 
         return ifStatement;
     },
